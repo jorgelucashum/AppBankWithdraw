8d0d2648aa5b8e34816fd13177c5700b0b3c0c01
Jorge Lucas <jorgelucashum@gmail.com>
2021-04-23 23:44:33 -0300

    Add project files.

---
+++ AppBankWithdraw/Entities/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppBankWithdraw.Entities.Exceptions;
+
+namespace AppBankWithdraw.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account()
+        {
+
+        }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+        
+        public void Deposit(double value)
+        {
+            Balance += value;
+        }
+
+        public void Withdraw(double value)
+        {
+            if (Balance <= 0 || value > Balance)
+            {
+                throw new WithdrawException("Não há saldo suficiente para realizar a operação.");
+            }
+            if(value > WithdrawLimit )
+            {
+                throw new WithdrawException("O saque ultrapassa o limite da conta.");
+            }
+            Balance -= value;
+        }
+
+        public override string ToString()
+        {
+            return $"Número da conta: {Number}, Titular: {Holder}, Saldo: R${Balance.ToString("F2")}, Limite de saque: R${WithdrawLimit.ToString("F2")}.";
+        }
+
+    }
+}
---
+++ AppBankWithdraw/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppBankWithdraw.Entities;
+using AppBankWithdraw.Entities.Exceptions;
+
+namespace AppBankWithdraw
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Digite os dados da sua conta: ");
+                Console.Write("Número: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.Write("Titular: ");
+                string holder = Console.ReadLine();
+                Console.Write("Saldo inicial: ");
+                double balance = double.Parse(Console.ReadLine());
+                Console.Write("Limite de saque: ");
+                double withdrawLimit = double.Parse(Console.ReadLine());
+
+                Account account = new Account(n, holder, balance, withdrawLimit);
+                Console.WriteLine("\nDados da conta:\n" + account + "\n");
+
+                Console.Write("Digite um valor para sacar: ");
+                double value = double.Parse(Console.ReadLine());
+                account.Withdraw(value);
+                Console.WriteLine("\nDados da conta:\n" + account + "\n");
+            }
+            catch(WithdrawException e)
+            {
+                Console.WriteLine("Erro ao realizar o saque: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro inesperado: " + e.Message);
+            }
+
+        }
+    }
+}
